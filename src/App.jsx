e9c30e7b035ac666d9d942adc17d944ad289dbfa
Yongmin0423 <yym21345@gmail.com>
2025-04-01 21:08:22 +0900

    user Choice

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+// import { useState } from "react";
+import styled from "styled-components";
+import Box from "./components/Box";
+import { useState } from "react";
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  width: 100vw;
+`;
+
+const BoxContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 20px;
+  height: 50vh;
+`;
+
+const BtnContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-top: 20px;
+`;
+
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://www.nicepng.com/png/full/6-61708_rock-rock-paper-scissors-clipart.png",
+  },
+  scissors: {
+    name: "Scissors",
+    img: "src/img/scissors.png",
+  },
+  paper: {
+    name: "Paper",
+    img: "src/img/paper.png",
+  },
+};
+
+function App() {
+  const [userSelect, setUserSelect] = useState(null);
+
+  const playGame = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+  };
+  return (
+    <Wrap>
+      <BoxContainer>
+        <Box title="You" userSelect={userSelect} />
+        <Box title="Computer" />
+      </BoxContainer>
+      <BtnContainer>
+        <button onClick={() => playGame("rock")}>Rock</button>
+        <button onClick={() => playGame("scissors")}>Scissors</button>
+        <button onClick={() => playGame("paper")}>Paper</button>
+      </BtnContainer>
+    </Wrap>
+  );
+}
+
+export default App;
---
+++ src/components/Box.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 40%;
+  height: 100%;
+  img {
+    width: 50%;
+    aspect-ratio: 1/1;
+  }
+`;
+
+function Box({ title, userSelect }) {
+  console.log("User", userSelect);
+  return (
+    <>
+      <Container>
+        <h1>{title}</h1>
+        <img src={userSelect?.img || ""} />
+        <p>result</p>
+      </Container>
+    </>
+  );
+}
+
+export default Box;
